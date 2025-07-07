c0877b2978d918d691b469528c414f41374d1b76
lfotev96 <lfotev96@gmail.com>
2025-07-07 21:16:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def toggle_task(task_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Взимаме PORT от Render, иначе 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
