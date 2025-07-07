a314c5da5166f6132f324db6bf400f29f3aa05c1
lfotev96 <lfotev96@gmail.com>
2025-07-07 20:20:44 +0300

    Смяна на порт

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def download(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
