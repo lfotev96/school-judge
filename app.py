c4a6c8b5cee7272119a2269077aafd512a860c5a
lfotev96 <lfotev96@gmail.com>
2025-07-08 12:44:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -122,4 +122,7 @@ def uploaded_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if not os.path.exists(DATABASE):
+        init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
